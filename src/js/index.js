11321b0f26bf3a7771d0569a3ac4745deb7494bc
Yurij Kluchevsky <info@rightblog.ru>
2019-02-06 00:16:01 +0300

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,5 @@
-import axios from 'axios'
+import Search from "./models/Search"
 
-const proxy = 'http://cors-anywhere.herokuapp.com/'
-const apiUrl = 'https://www.food2fork.com/api/search'
-const apiKey = 'aaaaaa01ea60c8eebfa0c54460ebfb8e022b35'
-
-async function getResults(query){
-    try {
-        const res = await axios(`${proxy}${apiUrl}?key=${apiKey}&q=${query}`)
-        const recipes = res.data.recipes
-        console.log(recipes)
-    } catch (error) {
-        alert(error);
-    }
-}
-getResults('pizza')
-
+const search = new Search('pizza')
+// console.log(search)
+search.getResults();
