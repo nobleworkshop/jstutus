1f624d439e9f08ec18e549291be8195f33be943f
Yurij Kluchevsky <info@rightblog.ru>
2019-02-06 11:03:41 +0300

    commit

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -3,16 +3,18 @@ import axios from 'axios';
 export default class Search {
 	constructor(query){
 		this.query = query;
+		console.log(this.query);
 	}
 
 	async getResults(){
 		const proxy = 'http://cors-anywhere.herokuapp.com/'
 		const apiUrl = 'https://www.food2fork.com/api/search'
-		// const apiKey = 'aaaaaa01ea60c8eebfa0c54460ebfb8e022b35'
-		const apiKey = 'd155648b07b1681a3f70ac98ac385763'
+		const apiKey = '01ea60c8eebfa0c54460ebfb8e022b35'
+		// const apiKey = 'd155648b07b1681a3f70ac98ac385763'
 	    try {
 	        const res = await axios(`${proxy}${apiUrl}?key=${apiKey}&q=${this.query}`)
 	        this.result = res.data.recipes
+	        console.log(this.result);
 	    } catch (error) {
 	        alert(error);
 	    }
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,28 @@
 export const elements = {
 	searchForm: document.querySelector('.search'),
 	searchInput: document.querySelector('.search__field'),
+	searchRes: document.querySelector('.results'),
 	searchResList: document.querySelector('.results__list')
+}
+
+export const elementStrings = {
+	loader: 'loader'
+}
+
+export const renderLoader = parent => {
+	const loader = `
+		<div class="${elementStrings.loader}">
+			<svg>
+				<use href="img/icons.svg#icon-cw"></use>
+			</svg>
+		</div>
+	`;
+
+	// https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
+	parent.insertAdjacentHTML('afterbegin', loader)
+}
+
+export const clearLoader = parent => {
+	const loaderElement = document.querySelector(`.${elementStrings.loader}`)
+	if ( loaderElement ) loaderElement.parentElement.removeChild(loaderElement)
 }
